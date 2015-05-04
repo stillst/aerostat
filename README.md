baf056bd3b91b7226190673459882a7a9f1d32ea
Stanislav Gavrilov <stas.a.gavrilov@gmail.com>
2015-05-04 13:57:44 +0300

    Add bower

---
+++ README.md
@@ -0,0 +1,11 @@
+> This is training project from epixx.ru.
+> For the instalation you need to do the folowing.
+1. git clone https://github.com/stillst/aerostat.git
+2. npm i
+3. bower i
+4. gulp 
+> If you want to compile production version you need to do:
+5. gulp dist
+6. gulp connect-dist
+> If you want to clean directory for production version you need to do:
+7. gulp clean
---
+++ gulpfile.js
@@ -0,0 +1,102 @@
+var gulp = require('gulp'),
+	connect = require('gulp-connect'),
+	opn = require('opn'),
+	wiredep = require('wiredep').stream,
+	useref = require('gulp-useref'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
+	clean = require('gulp-clean'),
+	less = require('gulp-less'),
+	notify = require('gulp-notify'),
+	minifyCss = require('gulp-minify-css');
+
+// Очистка папки DIST
+gulp.task('clean', function () {
+    return gulp.src('dist/', {read: false})
+        .pipe(clean())
+        .pipe(notify('dist cleaned'));
+});
+
+// Копирование статики
+gulp.task('copy', function () {
+    return gulp.src('src/img/*')
+     .pipe(gulp.dest('dist/img/'));   
+});
+
+
+// Склейка, минификация для js и css. А также изменение путей до css* и js* и копирирование их в dist 
+gulp.task('dist', function () {
+    var assets = useref.assets();
+    return gulp.src('src/*.html')
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
+});
+
+// Bower
+gulp.task('bower', function () { 
+	gulp.src('src/*.html')
+		.pipe(wiredep({
+			directory: 'src/bower'
+		}))
+		.pipe(gulp.dest('src'));
+});
+
+// Запуск сервера
+gulp.task('connect', function() {
+	connect.server({
+		root: 'src',
+		livereload: true
+	});
+	opn('http://localhost:8080')
+});
+
+// Запуск сервера
+gulp.task('connect-dist', function() {
+	connect.server({
+		root: 'dist'
+	});
+	opn('http://localhost:8080');
+});
+
+// Работа с html
+gulp.task('html', function () {
+	gulp.src('./src/*.html')
+	.pipe(connect.reload());
+});
+
+// Работа с CSS
+gulp.task('css', function () {
+	gulp.src('./src/css/*.css')
+	.pipe(connect.reload());
+});
+
+// Работа с JS
+gulp.task('js', function () {
+	gulp.src('./src/js/*.js')
+	.pipe(connect.reload());
+});
+
+//Компиляция LESS 
+gulp.task('less', function () {
+    return  gulp.src('src/less/style.less')
+    .pipe(less())
+    .pipe(gulp.dest('src/css'));
+});
+
+// Слежка
+gulp.task('watch', function () {
+	gulp.watch(['./src/*.html'], ['html']);
+	gulp.watch(['./src/css/*.css'], ['css']);
+	gulp.watch(['./src/js/*.js'], ['js']);
+	gulp.watch(['./src/less/**/*.less'], ['less']);
+});
+
+// Задача по-умолчанию 
+gulp.task('default', ['connect', 'watch']);
+
+// Сборка проекта
+gulp.task('compile', ['dist', 'copy']);
