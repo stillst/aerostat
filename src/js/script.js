50a67bee63dab1067150e92b5cca4cde84ed6438
stillst <stas.a.gavrilov@gmail.com>
2015-05-06 15:21:48 +0400

    Add gallery

--- src/js/script.js
+++ src/js/script.js
@@ -3,10 +3,16 @@ $(document).ready(function(){
   {
     $('.bxslider').bxSlider(
     {
+	    auto: true,
 	    slideWidth: 400,
 	    minSlides: 4,
 	    maxSlides: 4,
-	    slideMargin: 1
+	    slideMargin: 1,
+	    pager:false,
+	    nextSelector: '.gallery--controls__next',
+	    nextText:'<i class="fa  fa-2x fa-chevron-right"></i>',
+  		prevSelector: '.gallery--controls__prev',
+  		prevText: '<i class="fa  fa-2x fa-chevron-left"></i>'
 	});
   });
 });
